6fb9507d6b3cd00472d683a3138f1db20130fb63
hamo25salama2004-droid <hamo25salama2004@gmail.com>
2025-12-02 13:39:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ import streamlit as st
 st.header("1.الزر الاساسي")
 if st.button("اضغط هنا للترحيب"):
   st.write("اهلا بك في عالم البرمجة بلغة بايثون")
+
+st.header("اظهار/اخفاء")
+if st.checkbox("عرض التفاصيل"):
+  st.info("هذه هي التفاصل المخفية")
